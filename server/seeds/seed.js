b787fcc5cde9adee4189ae8deec990c04ff03e5b
Charles Parames <55605195+popokola@users.noreply.github.com>
2023-06-19 17:26:10 +0200

    Update seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,55 +1,45 @@
-const {
-    Sequelize,
-    DataTypes
-} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // Define your Sequelize configuration
 const sequelize = new Sequelize('testdb', 'postgres', 'postgres', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT || 5432,
+  dialect: 'postgres',
 });
 
 // Define your model for the "users" table
 const User = sequelize.define('User', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 // Define your seed data objects
-const seedData = [{
-        name: 'John Doe',
-        email: 'john@example.com'
-    },
-    {
-        name: 'Jane Smith',
-        email: 'jane@example.com'
-    },
-    // ...
+const seedData = [
+  { name: 'John Doe', email: 'john@example.com' },
+  { name: 'Jane Smith', email: 'jane@example.com' },
+  // ...
 ];
 
 async function seed() {
-    try {
-        // Sync the model with the database
-        await User.sync({
-            force: true
-        });
+  try {
+    // Sync the model with the database
+    await User.sync({ force: true });
 
-        // Insert seed data into the table
-        await User.bulkCreate(seedData);
+    // Insert seed data into the table
+    await User.bulkCreate(seedData);
 
-        console.log('Seed data inserted successfully');
+    console.log('Seed data inserted successfully');
 
-        await sequelize.close();
-    } catch (error) {
-        console.error('Error seeding the database:', error);
-    }
+    await sequelize.close();
+  } catch (error) {
+    console.error('Error seeding the database:', error);
+  }
 }
 
 seed();
