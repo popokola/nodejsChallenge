c0cd293515a89ea5a768bc2144955f2079c9f054
Charles Parames <55605195+popokola@users.noreply.github.com>
2023-06-20 09:33:57 +0000

    SSE refont

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ Object.entries(config).forEach(([provider, value]) => {
 
 
 //app.use(require("./routes/sse")());
-app.use(sse());
+//app.use(sse());
 app.use(require("./routes/registration")(userService));
 app.use(require("./routes/security")(userService, factory));
 app.use("/products", cookieJwtAuth, new GenericRouter(new GenericController(productService)));
@@ -49,8 +49,56 @@ app.use("/products", cookieJwtAuth, new GenericRouter(new GenericController(prod
 app.use("/users2", require("./routes/user"));
 app.use("/users", cookieJwtAuth, new GenericRouter(new GenericController(userService)));
 
-app.get("/sse", (req, res) => {
-    res.sseSetup();
+const Redis = require('ioredis');
+let redisSubscriber = null;
+app.get("/sse/:id", (req, res) => {
+
+  if (req.httpVersionMajor === 1) {
+    res.setHeader('Connection', 'keep-alive');
+  }
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+
+
+  const intervalId = setInterval(() => {
+    res.write(':\n\n');
+  }, 1000);
+
+  if (!redisSubscriber) {
+    redisSubscriber = new Redis(process.env.REDIS_CONN_URl);
+
+    // Event listener for receiving messages from Redis
+    redisSubscriber.on('message', (channel, message) => {
+      // Send the message to the client through SSE
+      const messageObject = JSON.parse(message);
+      console.log(messageObject, req.params.id);
+
+      if (messageObject.userId == req.params.id) {
+        console.log( req.params.id, messageObject);
+        res.write(`data: ${message}\n\n`);
+
+      }
+
+        
+    });
+  }
+
+
+  // Subscribe to the Redis channel
+  const channel = 'orders';
+
+  // Subscribe to the Redis channel
+  redisSubscriber.subscribe(channel);  
+
+
+  res.on('close', () => {
+    clearInterval(intervalId);
+    res.end();
+    redisSubscriber.unsubscribe(channel);
+    redisSubscriber.quit();
+    redisSubscriber = null;
+  });
+
 });
 
 app.get("/", (req, res) => {
@@ -62,15 +110,25 @@ app.post("/", (req, res) => {
   res.json(req.body);
 });
 
-const userServicetest = require('./services/userTest');
-app.use('api/user', new GenericRouter(new GenericController(userServicetest)));
 
 app.get("/test", async (req, res) => {
+  let redisPublisher = new Redis(process.env.REDIS_CONN_URl);
   res.send("Hello world");
   const response = await fetch('https://zenquotes.io/api/random');
   const data = await response.json();
   const quote = data[0].q;
-  res.sendSSE({id: process.pid, name: quote } );
+
+  const messageObject = {
+    userId: 3,
+    quote,
+  };
+
+  const messageObject2 = {
+    userId: 4,
+    quote,
+  };
+  redisPublisher.publish('orders', JSON.stringify(messageObject));
+  redisPublisher.publish('orders', JSON.stringify(messageObject2));
 });
 
 app.get('/callback', async (req, res) => {
